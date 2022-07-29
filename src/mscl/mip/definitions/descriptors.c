a9475b4cdf6c436efd25643f88ffd0125bac6985
microstrain-sam <105877710+microstrain-sam@users.noreply.github.com>
2022-07-29 11:44:08 -0400

    Add functions for checking descriptors.

--- src/mscl/mip/definitions/descriptors.c
+++ src/mscl/mip/definitions/descriptors.c
@@ -10,17 +10,120 @@ extern "C" {
 
 
 ////////////////////////////////////////////////////////////////////////////////
+///@brief Determines if the descriptor set is valid.
+///
+///@param descriptor_set
+///
+///@returns true if the descriptor set is valid.
+///
+bool is_valid_descriptor_set(uint8_t descriptor_set)
+{
+    return descriptor_set != MIP_INVALID_DESCRIPTOR_SET;
+}
+
+////////////////////////////////////////////////////////////////////////////////
 ///@brief Determines if the descriptor set represents some kind of data.
 ///
 ///@param descriptor_set
 ///
 ///@returns true if the descriptor set represents data.
-///@returns false otherwise.
 ///
 bool is_data_descriptor_set(uint8_t descriptor_set)
 {
-    return (descriptor_set >= 0x80) && (descriptor_set < 0xF0);
+    return (descriptor_set >= MIP_DATA_DESCRIPTOR_SET_START) && (descriptor_set < MIP_RESERVED_DESCRIPTOR_SET_START);
 }
+
+////////////////////////////////////////////////////////////////////////////////
+///@brief Determines if the descriptor set contains commands.
+///
+///@param descriptor_set
+///
+///@returns true if the descriptor set contains commands.
+///
+bool is_cmd_descriptor_set(uint8_t descriptor_set)
+{
+    return (descriptor_set < MIP_DATA_DESCRIPTOR_SET_START);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+///@brief Determines if the descriptor is reserved for special purposes.
+///
+///@param descriptor_set
+///
+///@returns true if the descriptor set is reserved.
+///
+bool is_reserved_descriptor_set(uint8_t descriptor_set)
+{
+    return (descriptor_set >= MIP_RESERVED_DESCRIPTOR_SET_START);
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+///@brief Determines if the field descriptor is valid.
+///
+///@param field_descriptor
+///
+///@returns true if the field descriptor is valid.
+///
+bool is_valid_descriptor(uint8_t field_descriptor)
+{
+    return field_descriptor != MIP_INVALID_FIELD_DESCRIPTOR;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+///@brief Determines if the field descriptor is a command.
+///
+///@param field_descriptor
+///
+///@returns true if the field descriptor represents a command.
+///
+bool is_command_descriptor(uint8_t field_descriptor)
+{
+    return (field_descriptor < MIP_RESPONSE_DESCRIPTOR_START);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+///@brief Determines if the field descriptor is for an ack/nack reply.
+///
+///@param field_descriptor
+///
+///@returns true if the field descriptor represents an ack/nack reply code.
+///
+bool is_reply_descriptor(uint8_t field_descriptor)
+{
+    return (field_descriptor == MIP_REPLY_DESCRIPTOR);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+///@brief Determines if the field descriptor contains response data from a
+///       command.
+///
+/// The descriptor set is assumed to be a command set.
+///
+///@param field_descriptor
+///
+///@returns true if the associated field contains response data.
+///
+bool is_response_descriptor(uint8_t field_descriptor)
+{
+    return field_descriptor >= MIP_RESPONSE_DESCRIPTOR_START && !is_reserved_field_descriptor(field_descriptor);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+///@brief Determines if the field descriptor is reserved.
+///
+/// The descriptor set is assumed to be a command set.
+///
+///@param field_descriptor
+///
+///@returns true if the associated field is neither a command nor response.
+///
+bool is_reserved_descriptor(uint8_t field_descriptor)
+{
+    return ((field_descriptor|MIP_RESPONSE_DESCRIPTOR_START) >= MIP_RESERVED_DESCRIPTOR_START);
+}
+
 
 
 size_t insert_mip_function_selector(uint8_t* buffer, size_t buffer_size, size_t offset, enum mip_function_selector self)
