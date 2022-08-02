a823dbe022541fc14b3cc48d1e0cc5a26b779ffd
microstrain-sam <105877710+microstrain-sam@users.noreply.github.com>
2022-08-02 17:46:50 -0400

    MSCLLITE-31 Add global include file.

---
+++ src/mscl/mip/mip_all.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "mip_cmdqueue.h"
+#include "mip_dispatch.h"
+#include "mip_field.h"
+#include "mip_interface.h"
+#include "mip_offsets.h"
+#include "mip_packet.h"
+#include "mip_parser.h"
+#include "definitions/descriptors.h"
+
+#ifdef __cplusplus
+
+#include "mip.hpp"
+#include "mip_device.hpp"
+
+#endif // __cplusplus
