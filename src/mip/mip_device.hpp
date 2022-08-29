e37a6b1b3919f240425e5a0554b507698bf8b30e
microstrain-sam <105877710+microstrain-sam@users.noreply.github.com>
2022-08-29 13:21:44 -0400

    Add PendingCmd documentation and fix some errors.

--- src/mip/mip_device.hpp
+++ src/mip/mip_device.hpp
@@ -9,6 +9,10 @@
 namespace mip
 {
 
+////////////////////////////////////////////////////////////////////////////////
+///@addtogroup mip_interface
+///@{
+
 using DispatchHandler = C::mip_dispatch_handler;
 
 struct Dispatcher : public C::mip_dispatcher
@@ -20,6 +24,9 @@ struct Dispatcher : public C::mip_dispatcher
 };
 
 
+////////////////////////////////////////////////////////////////////////////////
+///@brief C++ wrapper around a command queue.
+///
 struct CmdQueue : public C::mip_cmd_queue
 {
     CmdQueue(Timeout baseReplyTimeout=1000) { C::mip_cmd_queue_init(this, baseReplyTimeout); }
@@ -38,27 +45,72 @@ struct CmdQueue : public C::mip_cmd_queue
 
     void processPacket(const C::mip_packet& packet, Timestamp timestamp) { C::mip_cmd_queue_process_packet(this, &packet, timestamp); }
 };
+static_assert(sizeof(CmdQueue) == sizeof(C::mip_cmd_queue), "CmdQueue must not have additional data members.");
 
+
+////////////////////////////////////////////////////////////////////////////////
+///@brief C++ class representing the state of a MIP command.
+///
 struct PendingCmd : public C::mip_pending_cmd
 {
+    ///@brief Create a null pending command in the CmdResult::NONE state.
+    ///
     PendingCmd() { std::memset(static_cast<C::mip_pending_cmd*>(this), 0, sizeof(C::mip_pending_cmd)); }
+
+    ///@brief Create a pending command for the given descriptor pair.
+    ///
+    ///@param descriptorSet   MIP descriptor set for the command.
+    ///@param fieldDescriptor MIP field descriptor for the command.
+    ///@param additionalTime  Optional additional time to allow for the device to process the command. Default 0.
+    ///
     PendingCmd(uint8_t descriptorSet, uint8_t fieldDescriptor, Timeout additionalTime=0) { C::mip_pending_cmd_init_with_timeout(this, descriptorSet, fieldDescriptor, additionalTime); }
+
+    ///@brief Create a pending command with expected response.
+    ///
+    ///@param descriptorSet      MIP descriptor set for the command.
+    ///@param fieldDescriptor    MIP field descriptor for the command.
+    ///@param responseDescriptor MIP field descriptor for the response.
+    ///@param responseBuffer     A buffer used for the command response data. Must be big enough for the expected response. You can reuse the buffer used to send the command.
+    ///@param responseBufferSize Length of responseBuffer in bytes.
+    ///@param additionalTime     Optional additional time to allow for the device to process the command. Default 0.
+    ///
     PendingCmd(uint8_t descriptorSet, uint8_t fieldDescriptor, uint8_t responseDescriptor, uint8_t* responseBuffer, uint8_t responseBufferSize, Timeout additionalTime) { C::mip_pending_cmd_init_full(this, descriptorSet, fieldDescriptor, responseDescriptor, responseBuffer, responseBufferSize, additionalTime); }
 
+    ///@brief Create a pending command given the actual command struct.
+    ///
+    ///@param cmd The C++ command struct (this must be the C++ version of the struct, the C struct will not work). It need not be fully populated; this parameter is unused except for its type information.
+    ///@param additionalTime     Optional additional time to allow for the device to process the command. Default 0.
+    ///
     template<class Cmd>
     PendingCmd(const Cmd& cmd, Timeout additionalTime=0) : PendingCmd(cmd.descriptorSet, cmd.fieldDescriptor, additionalTime) {}
 
+    ///@brief Create a pending command given the actual command struct and a response buffer.
+    ///
+    ///@param cmd                The C++ command struct (this must be the C++ version of the struct, the C struct will not work). It need not be fully populated; this parameter is unused except for its type information.
+    ///@param responseBuffer     A buffer used for the command response data. Must be big enough for the expected response. You can reuse the buffer used to send the command.
+    ///@param responseBufferSize Length of responseBuffer in bytes.
+    ///@param additionalTime     Optional additional time to allow for the device to process the command. Default 0.
+    ///
     template<class Cmd>
-    PendingCmd(const Cmd& cmd, const typename Cmd::Response& response, uint8_t* responseBuffer, uint8_t responseBufferSize, Timeout additionalTime=0) : PendingCmd(cmd.descriptorSet, cmd.fieldDescriptor, response.fieldDescriptor, responseBuffer, responseBufferSize, additionalTime) {}
+    PendingCmd(const Cmd& cmd, uint8_t* responseBuffer, uint8_t responseBufferSize, Timeout additionalTime=0) : PendingCmd(Cmd::DESCRIPTOR_SET, Cmd::FIELD_DESCRIPTOR, Cmd::Response::FIELD_DESCRIPTOR, responseBuffer, responseBufferSize, additionalTime) {}
 
+    ///@brief Disable copying and moving. Once queued, a pending command must remain in the same memory location.
+    ///
     PendingCmd(const PendingCmd&) = delete;
     PendingCmd& operator=(const PendingCmd&) = delete;
 
+    ///@brief Sanity check that the PendingCmd is not deallocated while still in the queue.
+    ///
     ~PendingCmd() { CmdResult tmp = status(); assert(tmp.isFinished() || tmp==CmdResult::STATUS_NONE); (void)tmp; }
 
+    ///@brief Gets the status of the pending command.
+    ///
     CmdResult status() const { return C::mip_pending_cmd_status(this); }
 
+    ///@copydoc mip_pending_cmd_response
     const uint8_t* response() const { return C::mip_pending_cmd_response(this); }
+
+    ///@copydoc mip_pending_cmd_response_length
     uint8_t responseLength() const { return C::mip_pending_cmd_response_length(this); }
 };
 
@@ -139,16 +191,6 @@ public:
     CmdResult      waitForReply(const C::mip_pending_cmd& cmd) { return C::mip_interface_wait_for_reply(this, &cmd); }
 
     bool           defaultUpdate(bool blocking=false) { return C::mip_interface_default_update(this, blocking); }
-
-    // template<class Function>
-    // bool parseFromSource(Function function, unsigned int maxPackets=MIPPARSER_UNLIMITED_PACKETS) { return parseMipDataFromSource(parser(), function, maxPackets); }
-
-    //
-    // These must be implemented by a derived class
-    //
-
-    // virtual bool update() = 0;
-    // virtual bool sendToDevice(const uint8_t* data, size_t length) = 0;
 
     //
     // Data Callbacks
@@ -590,6 +632,7 @@ bool startCommand(C::mip_interface& device, C::mip_pending_cmd& pending, const C
 //}
 
 
+///@}
 ///@}
 ////////////////////////////////////////////////////////////////////////////////
 
