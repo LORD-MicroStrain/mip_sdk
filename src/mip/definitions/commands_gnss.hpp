2d9afa437b723df873d4bbb6ccadd491ecf4fd3d
microstrain-build <105877710+microstrain-sam@users.noreply.github.com>
2023-02-16 14:04:33 -0500

    Generate MIP definitions from branches/dev@54435.

--- src/mip/definitions/commands_gnss.hpp
+++ src/mip/definitions/commands_gnss.hpp
@@ -34,12 +34,10 @@ enum
     CMD_LIST_RECEIVERS             = 0x01,
     CMD_SIGNAL_CONFIGURATION       = 0x02,
     CMD_RTK_DONGLE_CONFIGURATION   = 0x10,
-    CMD_SPARTN_CONFIGURATION       = 0x20,
     
     REPLY_LIST_RECEIVERS           = 0x81,
     REPLY_SIGNAL_CONFIGURATION     = 0x82,
     REPLY_RTK_DONGLE_CONFIGURATION = 0x90,
-    REPLY_SPARTN_CONFIGURATION     = 0xA0,
 };
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -152,66 +150,6 @@ CmdResult readSignalConfiguration(C::mip_interface& device, uint8_t* gpsEnableOu
 CmdResult saveSignalConfiguration(C::mip_interface& device);
 CmdResult loadSignalConfiguration(C::mip_interface& device);
 CmdResult defaultSignalConfiguration(C::mip_interface& device);
-///@}
-///
-////////////////////////////////////////////////////////////////////////////////
-///@defgroup cpp_gnss_mip_gnss_spartn_configuration  (0x0E,0x20) Mip Gnss Spartn Configuration [CPP]
-/// Configure the SPARTN corrections service parameters.
-/// Notes:<br/>
-/// - Enable and type settings will only update after a power cycle <br/>
-/// - Type settings will only take effect after a power cycle <br/>
-/// - Key information can be updated while running
-///
-///@{
-
-struct MipGnssSpartnConfiguration
-{
-    static const uint8_t DESCRIPTOR_SET = ::mip::commands_gnss::DESCRIPTOR_SET;
-    static const uint8_t FIELD_DESCRIPTOR = ::mip::commands_gnss::CMD_SPARTN_CONFIGURATION;
-    
-    static const bool HAS_WRITE_FUNCTION = true;
-    static const bool HAS_READ_FUNCTION = true;
-    static const bool HAS_SAVE_FUNCTION = true;
-    static const bool HAS_LOAD_FUNCTION = true;
-    static const bool HAS_RESET_FUNCTION = true;
-    
-    FunctionSelector function = static_cast<FunctionSelector>(0);
-    uint8_t enable = 0; ///< Enable/Disable the SPARTN subsystem (0 = Disabled, 1 = Enabled)
-    uint8_t type = 0; ///< Connection type (0 - None, 1 = Network, 2 = L-Band)
-    uint32_t current_key_tow = 0; ///< The GPS time of week the current key is valid until
-    uint16_t current_key_week = 0; ///< The GPS week number the current key is valid until
-    uint8_t current_key[32] = {0}; ///< 32 character string of ASCII hex values for the current key (e.g. "bc" for 0xBC)
-    uint32_t next_key_tow = 0; ///< The GPS time of week the next key is valid until
-    uint16_t next_key_week = 0; ///< The GPS week number the next key is valid until
-    uint8_t next_key[32] = {0}; ///< 32 character string of ASCII hex valuesfor the next key (e.g. "bc" for 0xBC)
-    
-    struct Response
-    {
-        static const uint8_t DESCRIPTOR_SET = ::mip::commands_gnss::DESCRIPTOR_SET;
-        static const uint8_t FIELD_DESCRIPTOR = ::mip::commands_gnss::REPLY_SPARTN_CONFIGURATION;
-        
-        uint8_t enable = 0; ///< Enable/Disable the SPARTN subsystem (0 = Disabled, 1 = Enabled)
-        uint8_t type = 0; ///< Connection type (0 - None, 1 = Network, 2 = L-Band)
-        uint32_t current_key_tow = 0; ///< The GPS time of week the current key is valid until
-        uint16_t current_key_week = 0; ///< The GPS week number the current key is valid until
-        uint8_t current_key[32] = {0}; ///< 32 character string of ASCII hex values for the current key (e.g. "bc" for 0xBC)
-        uint32_t next_key_tow = 0; ///< The GPS time of week the next key is valid until
-        uint16_t next_key_week = 0; ///< The GPS week number the next key is valid until
-        uint8_t next_key[32] = {0}; ///< 32 character string of ASCII hex valuesfor the next key (e.g. "bc" for 0xBC)
-        
-    };
-};
-void insert(Serializer& serializer, const MipGnssSpartnConfiguration& self);
-void extract(Serializer& serializer, MipGnssSpartnConfiguration& self);
-
-void insert(Serializer& serializer, const MipGnssSpartnConfiguration::Response& self);
-void extract(Serializer& serializer, MipGnssSpartnConfiguration::Response& self);
-
-CmdResult writeMipGnssSpartnConfiguration(C::mip_interface& device, uint8_t enable, uint8_t type, uint32_t currentKeyTow, uint16_t currentKeyWeek, const uint8_t* currentKey, uint32_t nextKeyTow, uint16_t nextKeyWeek, const uint8_t* nextKey);
-CmdResult readMipGnssSpartnConfiguration(C::mip_interface& device, uint8_t* enableOut, uint8_t* typeOut, uint32_t* currentKeyTowOut, uint16_t* currentKeyWeekOut, uint8_t* currentKeyOut, uint32_t* nextKeyTowOut, uint16_t* nextKeyWeekOut, uint8_t* nextKeyOut);
-CmdResult saveMipGnssSpartnConfiguration(C::mip_interface& device);
-CmdResult loadMipGnssSpartnConfiguration(C::mip_interface& device);
-CmdResult defaultMipGnssSpartnConfiguration(C::mip_interface& device);
 ///@}
 ///
 ////////////////////////////////////////////////////////////////////////////////
