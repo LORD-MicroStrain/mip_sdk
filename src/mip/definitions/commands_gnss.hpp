b4da431af8b6bdb157300daaa74040629bb6b918
microstrain-sam <105877710+microstrain-sam@users.noreply.github.com>
2023-02-16 17:16:27 -0500

    Fix bad merge of gnss definitions.

--- src/mip/definitions/commands_gnss.hpp
+++ src/mip/definitions/commands_gnss.hpp
@@ -153,65 +153,6 @@ CmdResult defaultSignalConfiguration(C::mip_interface& device);
 ///@}
 ///
 ////////////////////////////////////////////////////////////////////////////////
-///@defgroup cpp_gnss_mip_gnss_spartn_configuration  (0x0E,0x20) Mip Gnss Spartn Configuration [CPP]
-/// Configure the SPARTN corrections service parameters.
-/// Notes:<br/>
-/// - Enable and type settings will only update after a power cycle <br/>
-/// - Key information can be updated while running
-///
-///@{
-
-struct MipGnssSpartnConfiguration
-{
-    static const uint8_t DESCRIPTOR_SET = ::mip::commands_gnss::DESCRIPTOR_SET;
-    static const uint8_t FIELD_DESCRIPTOR = ::mip::commands_gnss::CMD_SPARTN_CONFIGURATION;
-    
-    static const bool HAS_WRITE_FUNCTION = true;
-    static const bool HAS_READ_FUNCTION = true;
-    static const bool HAS_SAVE_FUNCTION = true;
-    static const bool HAS_LOAD_FUNCTION = true;
-    static const bool HAS_RESET_FUNCTION = true;
-    
-    FunctionSelector function = static_cast<FunctionSelector>(0);
-    uint8_t enable = 0; ///< Enable/Disable the SPARTN subsystem (0 = Disabled, 1 = Enabled)
-    uint8_t type = 0; ///< Connection type (1 = Network, 2 = L-Band)
-    uint32_t current_key_tow = 0; ///< The GPS time of week the current key is valid until
-    uint16_t current_key_week = 0; ///< The GPS week number the current key is valid until
-    uint8_t current_key[32] = {0}; ///< 32 character string of ASCII hex values for the current key (e.g. "bc" for 0xBC)
-    uint32_t next_key_tow = 0; ///< The GPS time of week the next key is valid until
-    uint16_t next_key_week = 0; ///< The GPS week number the next key is valid until
-    uint8_t next_key[32] = {0}; ///< 32 character string of ASCII hex valuesfor the next key (e.g. "bc" for 0xBC)
-    
-    struct Response
-    {
-        static const uint8_t DESCRIPTOR_SET = ::mip::commands_gnss::DESCRIPTOR_SET;
-        static const uint8_t FIELD_DESCRIPTOR = ::mip::commands_gnss::REPLY_SPARTN_CONFIGURATION;
-        
-        uint8_t enable = 0; ///< Enable/Disable the SPARTN subsystem (0 = Disabled, 1 = Enabled)
-        uint8_t type = 0; ///< Connection type (1 = Network, 2 = L-Band)
-        uint32_t current_key_tow = 0; ///< The GPS time of week the current key is valid until
-        uint16_t current_key_week = 0; ///< The GPS week number the current key is valid until
-        uint8_t current_key[32] = {0}; ///< 32 character string of ASCII hex values for the current key (e.g. "bc" for 0xBC)
-        uint32_t next_key_tow = 0; ///< The GPS time of week the next key is valid until
-        uint16_t next_key_week = 0; ///< The GPS week number the next key is valid until
-        uint8_t next_key[32] = {0}; ///< 32 character string of ASCII hex valuesfor the next key (e.g. "bc" for 0xBC)
-        
-    };
-};
-void insert(Serializer& serializer, const MipGnssSpartnConfiguration& self);
-void extract(Serializer& serializer, MipGnssSpartnConfiguration& self);
-
-void insert(Serializer& serializer, const MipGnssSpartnConfiguration::Response& self);
-void extract(Serializer& serializer, MipGnssSpartnConfiguration::Response& self);
-
-CmdResult writeMipGnssSpartnConfiguration(C::mip_interface& device, uint8_t enable, uint8_t type, uint32_t currentKeyTow, uint16_t currentKeyWeek, const uint8_t* currentKey, uint32_t nextKeyTow, uint16_t nextKeyWeek, const uint8_t* nextKey);
-CmdResult readMipGnssSpartnConfiguration(C::mip_interface& device, uint8_t* enableOut, uint8_t* typeOut, uint32_t* currentKeyTowOut, uint16_t* currentKeyWeekOut, uint8_t* currentKeyOut, uint32_t* nextKeyTowOut, uint16_t* nextKeyWeekOut, uint8_t* nextKeyOut);
-CmdResult saveMipGnssSpartnConfiguration(C::mip_interface& device);
-CmdResult loadMipGnssSpartnConfiguration(C::mip_interface& device);
-CmdResult defaultMipGnssSpartnConfiguration(C::mip_interface& device);
-///@}
-///
-////////////////////////////////////////////////////////////////////////////////
 ///@defgroup cpp_gnss_rtk_dongle_configuration  (0x0E,0x10) Rtk Dongle Configuration [CPP]
 /// Configure the communications with the RTK Dongle connected to the device.
 /// 
