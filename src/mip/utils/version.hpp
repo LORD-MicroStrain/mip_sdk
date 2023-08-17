54cde6f3e18301702080a67da8144de7cffaaf20
msclissa <melissa.gill@parker.com>
2023-08-17 17:07:21 -0400

    add Version interpreter from InertialConnect

---
+++ src/mip/utils/version.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <assert.h>
+#include <string>
+#include <cstdio>
+
+////////////////////////////////////////////////////////////////////////////////
+///@defgroup mip_serialization  MIP Serialization
+///
+///@brief Serialization Functions for reading and writing to byte buffers.
+///@{
+///@defgroup mip_serialization_c   MIP Serialization [C]
+///@defgroup mip_serialization_cpp MIP Serialization [CPP]
+///@}
+
+#ifdef __cplusplus
+#include <type_traits>
+
+namespace mip {
+namespace C {
+extern "C" {
+#endif // __cplusplus
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+///@addtogroup version_cpp
+///
+///@brief Version interpretation in C++.
+///
+///@{
+
+////////////////////////////////////////////////////////////////////////////////
+///@brief Structure used interpretting version strings.
+///
+///
+struct Version
+{
+    Version(uint16_t device_value=0) : m_value(device_value) {}
+    Version(uint8_t major_, uint8_t minor_, uint8_t patch_) : m_value(major_*100+minor_*10+patch_) { assert(major_<10 && minor_<10 && patch_<100); }
+
+    uint8_t major() const { return m_value / 100; }
+    uint8_t minor() const { return (m_value / 10) % 10;}
+    uint8_t patch() const { return m_value % 100; }
+    uint16_t bcd() const { return m_value; }
+
+    bool isDevVersion()     const { return major() == 0; }
+    bool isReleaseVersion() const { return major() >  0; }
+    bool isSpecialVersion() const { return major() >  1; }
+
+    bool operator==(Version other) const { return m_value == other.m_value; }
+    bool operator!=(Version other) const { return m_value != other.m_value; }
+    bool operator<=(Version other) const { return m_value <= other.m_value; }
+    bool operator>=(Version other) const { return m_value <= other.m_value; }
+    bool operator< (Version other) const { return m_value < other.m_value; }
+    bool operator> (Version other) const { return m_value < other.m_value; }
+
+    static constexpr size_t STRING_BUFFER_SIZE = 7+1;  // Including null
+    void toString(char* buffer) const { std::snprintf(buffer, STRING_BUFFER_SIZE, "%u.%u.%02u", major(), minor(), patch()); }
+    std::string toString() const { char buffer[STRING_BUFFER_SIZE]; toString(buffer); return std::string(buffer); }
+
+private:
+    uint16_t m_value = 0;
+};
+
+
+///@}
+///@}
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace mip
+#endif // __cplusplus
+
+////////////////////////////////////////////////////////////////////////////////
