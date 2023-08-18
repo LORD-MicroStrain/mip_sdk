7bade63edceb8418a254fec499be878075efb3d2
msclissa <melissa.gill@parker.com>
2023-08-18 17:51:29 -0400

    add model enum and info, improvements to version.hpp

---
+++ src/mip/mip_models.c
@@ -0,0 +1,82 @@
+#include "mip_models.h"
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+namespace mip {
+#endif // __cplusplus
+
+    enum Model getModelFromSerial(const char* serial)
+    {
+        char model_str[16];
+        bool found = false;
+        // The model number is just the portion of the serial before the dot
+        // serial number field is 16 chars
+        for (uint8_t i = 0; i < 16; i++)
+        {
+            model_str[i] = serial[i];
+            if (serial[i] == '.')
+            {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            return MODEL_UNKNOWN;
+        }
+
+        return atoi(model_str);
+    }
+
+#ifdef __cplusplus
+
+    std::string getModelNameFromSerial(const char* serial)
+    {
+        enum Model model = getModelFromSerial(serial);
+        switch (model)
+        {
+            case MODEL_3DM_DH3      : return "3DM-DH3";
+            case MODEL_3DM_GX3_15   : return "3DM-GX3-15";
+            case MODEL_3DM_GX3_25   : return "3DM-GX3-25";
+            case MODEL_3DM_GX3_35   : return "3DM-GX3-35";
+            case MODEL_3DM_GX3_45   : return "3DM-GX3-45";
+            case MODEL_3DM_RQ1_45_LT: return "3DM-RQ1-45_LT";
+            case MODEL_3DM_GX4_15   : return "3DM-GX4-15";
+            case MODEL_3DM_GX4_25   : return "3DM-GX4-25";
+            case MODEL_3DM_GX4_45   : return "3DM-GX4-45";
+            case MODEL_3DM_RQ1_45_ST: return "3DM-RQ1-45_ST";
+            case MODEL_3DM_GX5_10   : return "3DM-GX5-10";
+            case MODEL_3DM_GX5_15   : return "3DM-GX5-15";
+            case MODEL_3DM_GX5_25   : return "3DM-GX5-25";
+            case MODEL_3DM_GX5_35   : return "3DM-GX5-35";
+            case MODEL_3DM_GX5_45   : return "3DM-GX5-45";
+            case MODEL_3DM_CV5_10   : return "3DM-CV5-10";
+            case MODEL_3DM_CV5_15   : return "3DM-CV5-15";
+            case MODEL_3DM_CV5_25   : return "3DM-CV5-25";
+            case MODEL_3DM_CV5_45   : return "3DM-CV5-45";
+            case MODEL_3DM_GQ4_45   : return "3DM-GQ4-45";
+            case MODEL_3DM_CX5_45   : return "3DM-CX5-45";
+            case MODEL_3DM_CX5_35   : return "3DM-CX5-35";
+            case MODEL_3DM_CX5_25   : return "3DM-CX5-25";
+            case MODEL_3DM_CX5_15   : return "3DM-CX5-15";
+            case MODEL_3DM_CX5_10   : return "3DM-CX5-10";
+            case MODEL_3DM_CL5_15   : return "3DM-CL5-15";
+            case MODEL_3DM_CL5_25   : return "3DM-CL5-25";
+            case MODEL_3DM_GQ7      : return "3DM-GQ7";
+            case MODEL_3DM_RTK      : return "3DM-RTK";
+            case MODEL_3DM_CV7_AHRS : return "3DM-CV7-AHRS";
+            case MODEL_3DM_CV7_AR   : return "3DM-CV7-AR";
+            case MODEL_3DM_GV7_AHRS : return "3DM-GV7-AHRS";
+            case MODEL_3DM_GV7_AR   : return "3DM-GV7-AR";
+            case MODEL_3DM_GV7_INS  : return "3DM-GV7-INS";
+            case MODEL_3DM_CV7_INS  : return "3DM-CV7-INS";
+
+            default:
+            case MODEL_UNKNOWN      : return "";
+        }
+    }
+} // namespace mip
+#endif // __cplusplus
--- src/mip/utils/version.hpp
+++ src/mip/utils/version.hpp
@@ -3,6 +3,7 @@
 #include <assert.h>
 #include <string>
 #include <cstdio>
+#include <regex>
 
 ////////////////////////////////////////////////////////////////////////////////
 ///@defgroup mip_serialization  MIP Serialization
@@ -17,56 +18,76 @@
 #include <type_traits>
 
 namespace mip {
-#endif // __cplusplus
 
+    ////////////////////////////////////////////////////////////////////////////////
+    ///@addtogroup version_cpp
+    ///
+    ///@brief Version interpretation in C++.
+    ///
+    ///@{
 
+    ////////////////////////////////////////////////////////////////////////////////
+    ///@brief Structure used interpretting version strings.
+    ///
+    ///
+    struct Version
+    {
+        Version(uint16_t device_value=0) : m_value(device_value) {}
+        Version(uint8_t major_, uint8_t minor_, uint8_t patch_) : m_value(fromParts(major_, minor_, patch_)) { assert(major_<10 && minor_<10 && patch_<100); }
 
-////////////////////////////////////////////////////////////////////////////////
-///@addtogroup version_cpp
-///
-///@brief Version interpretation in C++.
-///
-///@{
+        uint8_t major() const { return m_value / 100; }
+        uint8_t minor() const { return (m_value / 10) % 10;}
+        uint8_t patch() const { return m_value % 100; }
+        uint16_t bcd() const { return m_value; }
 
-////////////////////////////////////////////////////////////////////////////////
-///@brief Structure used interpretting version strings.
-///
-///
-struct Version
-{
-    Version(uint16_t device_value=0) : m_value(device_value) {}
-    Version(uint8_t major_, uint8_t minor_, uint8_t patch_) : m_value(major_*100+minor_*10+patch_) { assert(major_<10 && minor_<10 && patch_<100); }
+        bool isDevVersion()     const { return major() == 0; }
+        bool isReleaseVersion() const { return major() >  0; }
+        bool isSpecialVersion() const { return major() >  1; }
 
-    uint8_t major() const { return m_value / 100; }
-    uint8_t minor() const { return (m_value / 10) % 10;}
-    uint8_t patch() const { return m_value % 100; }
-    uint16_t bcd() const { return m_value; }
+        bool operator==(Version other) const { return m_value == other.m_value; }
+        bool operator!=(Version other) const { return m_value != other.m_value; }
+        bool operator<=(Version other) const { return m_value <= other.m_value; }
+        bool operator>=(Version other) const { return m_value <= other.m_value; }
+        bool operator< (Version other) const { return m_value < other.m_value; }
+        bool operator> (Version other) const { return m_value < other.m_value; }
 
-    bool isDevVersion()     const { return major() == 0; }
-    bool isReleaseVersion() const { return major() >  0; }
-    bool isSpecialVersion() const { return major() >  1; }
+        static constexpr size_t STRING_BUFFER_SIZE = 7+1;  // Including null
+        void toString(char* buffer) const { std::snprintf(buffer, STRING_BUFFER_SIZE, "%u.%u.%02u", major(), minor(), patch()); }
+        std::string toString() const { char buffer[STRING_BUFFER_SIZE]; toString(buffer); return std::string(buffer); }
 
-    bool operator==(Version other) const { return m_value == other.m_value; }
-    bool operator!=(Version other) const { return m_value != other.m_value; }
-    bool operator<=(Version other) const { return m_value <= other.m_value; }
-    bool operator>=(Version other) const { return m_value <= other.m_value; }
-    bool operator< (Version other) const { return m_value < other.m_value; }
-    bool operator> (Version other) const { return m_value < other.m_value; }
+        bool fromString(std::string str)
+        {
+            // Pattern to extract only numbers Major.Minor.Patch. Anything else before or after is ignored
+            const std::regex pattern(R"((\d+)\.?(\d+)?\.?(\d+)?)");
 
-    static constexpr size_t STRING_BUFFER_SIZE = 7+1;  // Including null
-    void toString(char* buffer) const { std::snprintf(buffer, STRING_BUFFER_SIZE, "%u.%u.%02u", major(), minor(), patch()); }
-    std::string toString() const { char buffer[STRING_BUFFER_SIZE]; toString(buffer); return std::string(buffer); }
+            std::smatch match;
 
-private:
-    uint16_t m_value = 0;
-};
+            // Pattern will always find a Major number if it exists
+            if (std::regex_search(str, match, pattern))
+            {
+                uint8_t major = std::stoi(match[1]);
 
+                // Minor is optionally found
+                uint8_t minor = match[2].matched ? std::stoi(match[2]) : 0;
 
-///@}
-///@}
-////////////////////////////////////////////////////////////////////////////////
+                // Patch is optionally found
+                uint8_t patch = match[3].matched ? std::stoi(match[3]) : 0;
 
-#ifdef __cplusplus
+                m_value = fromParts(major, minor, patch);
+
+                return true;
+            }
+
+            return false;
+        }
+
+    private:
+        uint16_t m_value = 0;
+
+    private:
+        static uint16_t fromParts(uint8_t major, uint8_t minor, uint8_t patch) { return major * 100 + minor * 10 + patch; }
+    };
+
 } // namespace mip
 #endif // __cplusplus
 
