46730620b45f270e909de3ad52d0ccf4b0f252b8
microstrain-sam <105877710+microstrain-sam@users.noreply.github.com>
2024-04-22 18:17:41 -0400

    - Improve performance test for more data.

--- test/mip/mip_parser_performance.cpp
+++ test/mip/mip_parser_performance.cpp
@@ -1,6 +1,7 @@
 
 #include "mip/mip.hpp"
 
+#include <algorithm>
 #include <vector>
 #include <chrono>
 #include <numeric>
@@ -34,6 +35,7 @@ struct Test
     unsigned int num_iterations = 1;
     size_t num_packets = 0;
     std::vector<uint8_t> data;
+    std::vector<size_t> chunk_sizes;
 };
 
 
@@ -43,7 +45,8 @@ Test generate_pings()
 
     test.name = "Pings";
     test.num_packets = 200000;
-    test.num_iterations = 100;
+    test.num_iterations = 10;
+    test.chunk_sizes = {5, 8, 19};
 
     test.data.resize(sizeof(PING_PACKET)*test.num_packets);
 
@@ -57,9 +60,9 @@ Test generate_long_pkts()
 {
     Test test;
 
-    test.name = "Long Pkts";
+    test.name = "Long_Pkts";
     test.num_packets = 100'000;
-    test.num_iterations = 100;
+    test.num_iterations = 10;
 
     test.data.resize(sizeof(DATA_PACKET)*test.num_packets);
 
@@ -73,9 +76,9 @@ Test generate_interleaved()
 {
     Test test;
 
-    test.name = "InterleavedNMEA";
+    test.name = "Interleave_NMEA";
     test.num_packets = 100'000;
-    test.num_iterations = 100;
+    test.num_iterations = 10;
 
     const size_t INTERVAL = sizeof(DATA_PACKET) + sizeof(NMEA_SENTENCE);
     test.data.resize(INTERVAL*test.num_packets);
@@ -89,71 +92,153 @@ Test generate_interleaved()
     return test;
 }
 
-void run_parser(const Test& test)
-{
-    struct Stats
-    {
-        size_t num_bytes = 0;
-        size_t num_pkts  = 0;
-    };
+volatile bool dummy = false;
 
-    Stats stats;
+struct ChunkStats
+{
+    size_t chunk_size = 0;  // Size of chunks
+    size_t num_calls  = 0;  // Calls to parse per test iteration
+    float total_time = 0;   // Average total time per test iteration
+    float max_time   = 0;   // Max of any single call
+    float avg_time   = 0;   // Average of every call
+    float med_time   = 0;   // Median of every call
+};
+
+//constexpr float kahan_sum(const float* data, size_t count)
+//{
+//    float result = 0;
+//    float c = 0;
+//    for(size_t i=0; i<count; i++)
+//    {
+//        float y = data[i] - c;
+//        float t = result + y;
+//        c = (t - result) - y;
+//        result = t;
+//    }
+//    return result;
+//}
+
+
+ChunkStats chunked_test(const Test& test, size_t chunk_size)
+{
     auto callback = +[](void* v, const mip::PacketRef* p, mip::Timestamp)
     {
-        Stats& s = *static_cast<Stats*>(v);
-        s.num_bytes += p->totalLength();
-        s.num_pkts++;
+        *static_cast<size_t*>(v) += 1;
     };
-    mip::Parser parser(callback, &stats, MIP_PARSER_DEFAULT_TIMEOUT_MS);
+    size_t num_pkts  = 0;
+    mip::Parser parser(callback, &num_pkts, MIP_PARSER_DEFAULT_TIMEOUT_MS);
 
-    std::vector<float> timing(test.num_iterations, 0);
+
+    const size_t num_full_chunks = (chunk_size == 0) ? 1 : (test.data.size() / chunk_size);
+    std::vector<float> chunk_times(num_full_chunks * test.num_iterations);
 
     assert(test.num_iterations > 0);
-    for(unsigned int i=0; i<test.num_iterations; i++)
+    for(unsigned int i = 0; i < test.num_iterations; i++)
     {
         parser.reset();
+        num_pkts = 0;
 
-        auto start = std::chrono::steady_clock::now();
-        parser.parse(test.data.data(), test.data.size(), 0);
-        auto stop = std::chrono::steady_clock::now();
+        const size_t buffer_size = (chunk_size > 0) ? chunk_size : test.data.size();
+        const uint8_t *buffer = test.data.data();
 
-        std::chrono::duration<float> duration = stop - start;
+        for(size_t c = 0; c < num_full_chunks; c++)
+        {
+            auto start = std::chrono::steady_clock::now();
+            dummy = true;  // Hopefully prevent compiler/cpu from reordering clock and parse calls.
+            parser.parse(buffer, buffer_size, 0);
+            dummy = false;
+            auto stop = std::chrono::steady_clock::now();
 
-        timing[i] = duration.count();
+            std::chrono::duration<float> duration = stop - start;
+
+            chunk_times[i * num_full_chunks + c] = duration.count();
+            buffer += chunk_size;
+        }
+        if(chunk_size > 0)
+            parser.parse(test.data.data() + num_full_chunks * chunk_size, test.data.size() - num_full_chunks * chunk_size, 0);
+
+        if(num_pkts != test.num_packets)
+            fprintf(stderr, "Error: Got %zu packets but expected %zu!\n", num_pkts, test.num_packets);
     }
 
-    const float total_time = std::accumulate(timing.begin(), timing.end(), 0.0f);
-    const float avg_time = total_time / std::size(timing);
-    const float max_time = *std::max_element(timing.begin(), timing.end());
-    const float bytes_per_sec   = test.data.size()*test.num_iterations / total_time;
-    const float packets_per_sec = test.num_packets*test.num_iterations / total_time;
+    ChunkStats stats;
+    stats.chunk_size = chunk_size;
+    stats.num_calls  = num_full_chunks;
+    stats.total_time = std::accumulate(chunk_times.begin(), chunk_times.end(), 0.0) / test.num_iterations;  // Accumulate with double precision!
+    stats.avg_time   = stats.total_time / num_full_chunks;
+    stats.max_time   = *std::max_element(chunk_times.begin(), chunk_times.end());
 
-    std::printf(
-        "\nTest %s: %u iterations\n"
-        "  Bytes/iter:    %.3f MB\n"
-        "  Pkts/iter:     %.3f kPkt\n"
-        "  Total Packets: %.f / %.f%s MPkt\n"
-        "  Average Time:  %f ms\n"
-        "  Maximum Time:  %f ms\n"
-        "  Bytes/s:       %.3f MB/s\n"
-        "  Pkts/s:        %.3f MPkt/s\n",
-        test.name, test.num_iterations,
-        float(test.data.size())/1e6f,
-        float(test.num_packets)/1e3f,
-        float(stats.num_pkts)/1e6f, float(test.num_packets*test.num_iterations)/1e6f, (stats.num_pkts != test.num_packets*test.num_iterations ? "***" : ""),
-        avg_time*1e3f,
-        max_time*1e3f,
-        bytes_per_sec/1e6f,
-        packets_per_sec/1e6f
-    );
+    std::sort(chunk_times.begin(), chunk_times.end());
+    stats.med_time   = chunk_times[chunk_times.size()/2];
+
+    return stats;
 }
 
 
 int main(int argc, const char* argv[])
 {
-    run_parser(generate_long_pkts());
-    run_parser(generate_pings());
-    run_parser(generate_interleaved());
+    std::initializer_list<Test> tests = {
+        generate_pings(),
+        generate_long_pkts(),
+        generate_interleaved()
+    };
+
+    std::initializer_list<size_t> chunk_sizes = {5, 8, 19, 53, 127, 512, 1024, 8192};
+
+    std::vector<ChunkStats> stats(tests.size() * (chunk_sizes.size()+1));
+
+    size_t t = 0;
+    for(const Test& test : tests)
+    {
+        printf("Test %s...\n", test.name);
+
+        size_t s = 0;
+        for(size_t chunk_size : chunk_sizes)
+        {
+            printf("  Chunk size %zu\n", chunk_size);
+            stats[t * (chunk_sizes.size()+1) + s] = chunked_test(test, chunk_size);
+            s++;
+        }
+        printf("  Chunk size oo\n");
+        stats[t * (chunk_sizes.size()+1) + s] = chunked_test(test, test.data.size());
+
+        t++;
+    }
+
+    std::printf("\n\n"
+        "       Test          Total     Chunk      Parse     Mean       Median     Max        Though     Through  \n"
+        "       Name          Bytes     Size[B]    Calls     Time[us]   Time[us]   Time[us]    [MB/s]    [MPkt/s] \n"
+        "---------------------------------------------------------------------------------------------------------\n"
+    );
+
+    t=0;
+    for(const Test& test : tests)
+    {
+        size_t s=0;
+        for(size_t chunk_size : chunk_sizes)
+        {
+            const ChunkStats& stat = stats[t * (chunk_sizes.size()+1) + s];
+
+            std::printf("%16s   %8zu   %8zu   %8zu   %8.2f   %8.2f   %8.2f   %8.2f   %8.2f\n",
+                test.name, stat.chunk_size * stat.num_calls, chunk_size, stat.num_calls,
+                stat.avg_time*1e6f, stat.med_time*1e6f, stat.max_time*1e6f,
+                test.data.size() / stat.total_time / 1e6f,
+                test.num_packets / stat.total_time / 1e6f
+            );
+
+            s++;
+        }
+        const ChunkStats& stat = stats[t * (chunk_sizes.size()+1) + s];
+
+        std::printf("%16s   %8zu   infinite   %8zu   %8.2f   %8.2f   %8.2f   %8.2f   %8.2f\n",
+                test.name, stat.chunk_size * stat.num_calls, stat.num_calls,
+                stat.avg_time*1e6f, stat.med_time*1e6f, stat.max_time*1e6f,
+                test.data.size() / stat.total_time / 1e6f,
+                test.num_packets / stat.total_time / 1e6f
+        );
+
+        t++;
+    }
 
     return 0;
 }
