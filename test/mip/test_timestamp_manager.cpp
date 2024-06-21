2a369f56495013d5bc5ee5e148cf9399eefbd54f
Alex Farrell <ralexfarrell@outlook.com>
2024-06-21 11:11:30 -0400

    Added test case skeletons for getTimestamp

--- test/mip/test_timestamp_manager.cpp
+++ test/mip/test_timestamp_manager.cpp
@@ -1,5 +1,22 @@
+#include <mip/utils/timestamp_manager.hpp>
+
+#define SUCCESS 0
+#define    FAIL 1
+
+bool testGetTimestampBase() 
+{
+    return true;    
+}
+
+bool testGetTimestampTemplate()
+{
+    return true;
+}
+
 int main(int argc, const char* argv[])
 {
-    // TODO: Add test for getTimestamp.
-    return 0;
+    if (    !testGetTimestampBase()) { return FAIL; }
+    if (!testGetTimestampTemplate()) { return FAIL; }
+
+    return SUCCESS;
 }
