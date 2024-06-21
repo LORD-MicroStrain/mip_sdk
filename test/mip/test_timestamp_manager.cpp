e28fd3161262e11c21482800075a379bed5408ac
Alex Farrell <ralexfarrell@outlook.com>
2024-06-21 17:10:31 -0400

    Organized utility functions

--- test/mip/test_timestamp_manager.cpp
+++ test/mip/test_timestamp_manager.cpp
@@ -7,46 +7,18 @@
 
 #include <mip/utils/timestamp_manager.hpp>
 
-#define SUCCESS    0
-#define FAIL       1
-constexpr long long start_time = 9000000000;
+constexpr short success = 0;
+constexpr short fail = 1;
+constexpr long long start_time = 9000000000; // 9 seconds (in nanoseconds)
 
-
-// TODO: Organize
-template<typename ActualOutput, typename ExpectedOutput>
-    void outputCaseResults(const char* case_name, ActualOutput actual, ExpectedOutput expected)
-{
-    std::cout << 
-        "Failed: " << case_name << "\n" << 
-        "    ---> Actual: " << actual << "\n" <<
-        "    ---> Expected: " << expected << "\n";
-}
 
 template<typename DurationActual, typename DurationExpected>
-    bool testCase(std::string name, DurationActual actual, DurationExpected expected)
-{
-    const std::type_info& actual_type = typeid(actual);
-    const std::type_info& expected_type = typeid(expected);
-    
-    if (actual_type != expected_type)
-    {
-        outputCaseResults((name + "-type").c_str(), actual_type.name(), expected_type.name());
-        return false;
-    }
+    bool testCase(std::string name, DurationActual actual, DurationExpected expected);
 
-    if (actual != expected)
-    {
-        outputCaseResults((name + "-value").c_str(), actual.count(), expected.count());
-        return false;
-    }
-    
-    return true;
-}
+template<typename ActualOutput, typename ExpectedOutput>
+    void outputCaseResults(const char* case_name, ActualOutput actual, ExpectedOutput expected);
 
-mip::Seconds toSeconds(long long nanoseconds)
-{
-    return mip::Seconds(nanoseconds / 1000000000);
-}
+mip::Seconds toSeconds(long long nanoseconds);
 
 bool testGetTimestamp() 
 {
@@ -71,7 +43,48 @@ bool testGetTimestamp()
 
 int main(int argc, const char* argv[])
 {
-    if (!testGetTimestamp()) { return FAIL; }
+    if (!testGetTimestamp()) { return fail; }
 
-    return SUCCESS;
+    return success;
+}
+
+
+/**************************************************************************************/
+/* NOTE: The following are definitions for all utility declarations above. There are  */
+/*       no new declarations following this statement.                                */
+/**************************************************************************************/
+
+template<typename DurationActual, typename DurationExpected>
+    inline bool testCase(std::string name, DurationActual actual, DurationExpected expected)
+{
+    const std::type_info& actual_type = typeid(actual);
+    const std::type_info& expected_type = typeid(expected);
+    
+    if (actual_type != expected_type)
+    {
+        outputCaseResults((name + "-type").c_str(), actual_type.name(), expected_type.name());
+        return false;
+    }
+
+    if (actual != expected)
+    {
+        outputCaseResults((name + "-value").c_str(), actual.count(), expected.count());
+        return false;
+    }
+    
+    return true;
+}
+
+template<typename ActualOutput, typename ExpectedOutput>
+    inline void outputCaseResults(const char* case_name, ActualOutput actual, ExpectedOutput expected)
+{
+    std::cout << 
+        "Failed: " << case_name << "\n" << 
+        "    ---> Actual: " << actual << "\n" <<
+        "    ---> Expected: " << expected << "\n";
+}
+
+mip::Seconds toSeconds(long long nanoseconds)
+{
+    return mip::Seconds(nanoseconds / 1000000000);
 }
