2884badf5cf8fd80412719300ed82ef8def616e8
Alex Farrell <ralexfarrell@outlook.com>
2024-07-19 13:15:35 -0400

    Moved convertFrom and castTime to misc.hpp

--- src/mip/utils/time/timestamp.hpp
+++ src/mip/utils/time/timestamp.hpp
@@ -87,17 +87,6 @@ namespace mip
         template<typename DurationOut>
         DurationOut getTimeOfWeek();
         Nanoseconds getTimeOfWeek();
-        
-        // TODO: Move outside of class into separate utility.
-        // throws logic error
-        template<class DurationOut = Nanoseconds>
-        DurationOut convertFrom(const TimestampExperimental &reference);
-
-        // TODO: Move outside of class into separate utility.
-        // TODO: Update documentation.
-        /// Casts a timestamp duration to the given arithmetic type.
-        template<typename T, typename DurationIn>
-        T castTime(const DurationIn &time);
 
     private:
         const mip::TimeStandard &m_standard;
@@ -173,28 +162,5 @@ namespace mip
         }
 
         return std::chrono::duration_cast<DurationOut>(getTimeOfWeek());
-    }
-
-    template<class DurationOut>
-    inline DurationOut TimestampExperimental::convertFrom(const TimestampExperimental &reference)
-    {
-        Nanoseconds converted = m_standard.convertFromBase(reference.getTimestampBaseStandard());
-        if (converted < Nanoseconds(0))
-        {
-            throw std::logic_error("Converted timestamp < 0. Add exception handling with desired response!");
-        }
-        
-        return std::chrono::duration_cast<DurationOut>(converted);
-    }
-
-    template<typename T, typename DurationIn>
-    T TimestampExperimental::castTime(const DurationIn &time)
-    {
-        if (time < mip::Nanoseconds(0))
-        {
-            throw std::invalid_argument("Time < 0.");
-        }
-
-        return static_cast<T>(time.count());
     }
 } // namespace mip
