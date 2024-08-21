ad1c81be35a0e74830bf1c7990d7851f8405f784
microstrain-sam <105877710+microstrain-sam@users.noreply.github.com>
2024-08-21 17:24:40 -0400

    Fix mip::extract/insert.

--- src/cpp/mip/mip_serialization.hpp
+++ src/cpp/mip/mip_serialization.hpp
@@ -17,10 +17,26 @@ namespace serialization
     using namespace microstrain::serialization::big_endian;
 }
 
+using microstrain::insert;
+using microstrain::extract;
+
+// Explicitly overload insert/extract versions that take raw buffers
+// since the endianness can't be deduced from the arguments.
+
+template<class T>
+inline bool extract(T& object, const uint8_t* buffer, size_t bufferLength, size_t offset=0, bool exactSize=false)
+{
+    using namespace microstrain::serialization;
+    return extract<Endian::big>(object, buffer, bufferLength, offset, exactSize);
+}
+
+template<class T>
+inline bool insert(const T& object, uint8_t* buffer, size_t bufferLength, size_t offset=0)
+{
+    using namespace microstrain::serialization;
+    return insert<Endian::big>(object, buffer, bufferLength, offset);
+}
+
+
 } // namespace mip
 
-
-// These functions must be in the microstrain namespace in order to be seen as overloads.
-namespace microstrain
-{
-} // namespace microstrain
