ef2d82b132e1a6ec63fdebce8005c7149e77829b
Nick DaCosta <nick.dacosta@hbkworld.com>
2024-09-20 16:30:54 -0400

    Moved documentation builder to Linux

--- .devcontainer/docker_build.sh
+++ .devcontainer/docker_build.sh
@@ -8,6 +8,7 @@ set -e
 # Get some arguments from the user
 os="ubuntu"
 arch="amd64"
+build_docs=false
 while [[ $# -gt 0 ]]; do
   case $1 in
     --os)
@@ -20,6 +21,10 @@ while [[ $# -gt 0 ]]; do
       shift # past argument
       shift # past value
       ;;
+    --docs)
+      build_docs=true
+      shift # past argument
+      ;;
     *)
       shift # past argument
       ;;
@@ -31,9 +36,15 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 project_dir="${script_dir}/.."
 docker_project_dir="/home/microstrain/mipsdk"
 dockerfile="${script_dir}/Dockerfile.${os}"
-build_dir_name="build_${os}_${arch}"
 
-image_name="microstrain/mipsdk_${os}_builder:${arch}"
+if [ ${build_docs} = true ]; then
+  build_dir_name="build_docs"
+  image_name="microstrain/mipsdk_docs_builder:${arch}"
+else
+  build_dir_name="build_${os}_${arch}"
+  image_name="microstrain/mipsdk_${os}_builder:${arch}"
+fi
+
 
 # Build the docker image
 docker build \
@@ -49,6 +60,17 @@ if [ "${ISHUDSONBUILD}" != "True" ]; then
   docker_it_flags="-it"
 fi
 
+if [ ${build_docs} = true ]; then
+  configure_flags="-DMICROSTRAIN_BUILD_DOCUMENTATION=ON"
+  build_target="package_docs"
+else
+  configure_flags="\
+    -DMICROSTRAIN_BUILD_EXAMPLES=ON \
+    -DMICROSTRAIN_BUILD_PACKAGE=ON \
+    -DCMAKE_BUILD_TYPE=RELEASE"
+  build_target="package"
+fi
+
 # Run the build in the docker image
 docker run \
   --rm \
@@ -61,10 +83,7 @@ docker run \
     rm -rf ${docker_project_dir}/${build_dir_name}; \
     mkdir ${docker_project_dir}/${build_dir_name}; \
     cd ${docker_project_dir}/${build_dir_name}; \
-    cmake ${docker_project_dir} \
-        -DMICROSTRAIN_BUILD_EXAMPLES=ON \
-        -DMICROSTRAIN_BUILD_PACKAGE=ON \
-        -DCMAKE_BUILD_TYPE=RELEASE; \
+    cmake ${docker_project_dir} ${configure_flags}; \
     cmake --build . -j$(nproc); \
-    cmake --build . --target package; \
+    cmake --build . --target ${build_target}; \
   "
