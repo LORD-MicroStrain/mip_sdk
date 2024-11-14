4b7880339abb2c4861a8db740afbbc3af8a51169
microstrain-sam <105877710+microstrain-sam@users.noreply.github.com>
2024-11-14 16:06:13 -0500

    Document field metadata search functions and add shared data field check.

--- src/cpp/mip/metadata/mip_definitions.cpp
+++ src/cpp/mip/metadata/mip_definitions.cpp
@@ -10,6 +10,15 @@
 namespace mip::metadata
 {
 
+////////////////////////////////////////////////////////////////////////////////
+///@brief Searches for a descriptor set in a list of descriptor sets.
+///
+///@param descriptorSets List of descriptor sets to search.
+///@param descriptor     Descriptor set ID number.
+///
+///@returns A pointer to the descriptor set metadata if found.
+///@returns NULL if the specified ID isn't in the list.
+///
 const DescriptorSetInfo* findDescriptorSet(const DescriptorSetSpan& descriptorSets, uint8_t descriptor)
 {
     for(const DescriptorSetInfo* desc_set_info : descriptorSets)
@@ -20,20 +29,27 @@ const DescriptorSetInfo* findDescriptorSet(const DescriptorSetSpan& descriptorSe
     return nullptr;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+///@brief Searches for field metadata info by descriptor ID.
+///
+///@param descriptorSets List of descriptor sets to search.
+///@param descriptor     Descriptor set and field descriptor to search for.
+///
+///@returns A pointer to the field metadata if found.
+///@returns NULL if the specified descriptor isn't in the list.
+///
 const FieldInfo* findField(const DescriptorSetSpan& descriptorSets, mip::CompositeDescriptor descriptor)
 {
     if(mip::isDataDescriptorSet(descriptor.descriptorSet))
     {
         // Special case for shared data
         if(mip::isSharedDataFieldDescriptor(descriptor.fieldDescriptor))
-        {
-            return findField(DATA_SHARED, descriptor.fieldDescriptor);
-        }
+            return findField(DATA_SHARED, descriptor.fieldDescriptor, false);
     }
     else if(isCommandDescriptorSet(descriptor.descriptorSet))
     {
         // Special case for reply fields
-        if(isCommandDescriptorSet(descriptor.descriptorSet) && isReplyFieldDescriptor(descriptor.fieldDescriptor))
+        if(isReplyFieldDescriptor(descriptor.fieldDescriptor))
             return &MetadataFor<ReplyField>::value;
     }
 
@@ -41,14 +57,34 @@ const FieldInfo* findField(const DescriptorSetSpan& descriptorSets, mip::Composi
     if(!ds_info)
         return nullptr;
 
-    if(const FieldInfo* info = findField(*ds_info, descriptor.fieldDescriptor))
+    if(const FieldInfo* info = findField(*ds_info, descriptor.fieldDescriptor, false))
         return info;
 
     return nullptr;
 }
 
-const FieldInfo* findField(const DescriptorSetInfo& ds_info, uint8_t field_desc)
+////////////////////////////////////////////////////////////////////////////////
+///@brief Searches for field metadata info by descriptor ID.
+///
+/// You can use this function with a specific descriptor set constant, or after
+/// searching for one with findDescriptorSet.
+///
+///@param descriptorSets    Specific descriptor set to search.
+///@param descriptor        Field descriptor to search for.
+///@param check_shared_data If true and the descriptor is a shared data field,
+///                         searches the shared data set instead.
+///
+///@returns A pointer to the field metadata if found.
+///@returns NULL if the specified descriptor isn't in the list.
+///
+const FieldInfo* findField(const DescriptorSetInfo& ds_info, uint8_t field_desc, bool check_shared_data)
 {
+    if(check_shared_data && mip::isDataDescriptorSet(ds_info.descriptor))
+    {
+        if(mip::isSharedDataFieldDescriptor(field_desc))
+            return findField(DATA_SHARED, field_desc, false);
+    }
+
     // Binary search assumes fields are sorted by descriptor.
     auto it = std::lower_bound(
         ds_info.fields.begin(), ds_info.fields.end(), field_desc,
--- src/cpp/mip/metadata/mip_definitions.hpp
+++ src/cpp/mip/metadata/mip_definitions.hpp
@@ -10,10 +10,12 @@ namespace mip::metadata
 struct FieldInfo;
 struct DescriptorSetInfo;
 
-using DescriptorSetSpan = microstrain::Span<const DescriptorSetInfo* const>;
+// A list of const DescriptorSetInfo pointers.
+// This is a view of an array of const pointers to const DescriptorSetInfos.
+using DescriptorSetSpan = microstrain::Span<DescriptorSetInfo const* const>;
 
 const DescriptorSetInfo* findDescriptorSet(const DescriptorSetSpan& descriptorSets, uint8_t descriptor);
 const FieldInfo* findField(const DescriptorSetSpan& descriptorSets, mip::CompositeDescriptor descriptor);
-const FieldInfo* findField(const DescriptorSetInfo& ds_info, uint8_t field_desc);
+const FieldInfo* findField(const DescriptorSetInfo& ds_info, uint8_t field_desc, bool check_shared_data=true);
 
 } // namespace mip::metadata
