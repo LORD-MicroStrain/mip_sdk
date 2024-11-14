b9c4369081f2cdaa2e38e9de4370184b5a5f144a
microstrain-sam <105877710+microstrain-sam@users.noreply.github.com>
2024-11-14 15:33:05 -0500

    Cleanup old commented-out code.

--- src/cpp/mip/metadata/mip_definitions.cpp
+++ src/cpp/mip/metadata/mip_definitions.cpp
@@ -6,35 +6,9 @@
 #include <mip/metadata/definitions/data_shared.hpp>
 #include <mip/metadata/definitions/common.hpp>
 
+
 namespace mip::metadata
 {
-
-//void Definitions::registerField(const mip::metadata::FieldInfo* field)
-//{
-//    mFields.insert(field);
-//}
-//
-//void Definitions::registerDefinitions(const FieldSpan& fields)
-//{
-//    mFields.insert(fields.begin(), fields.end());
-//}
-//
-//void Definitions::registerDefinitions(const DescriptorSetSpan& ds)
-//{
-//    for(const FieldSpan& fields : ds)
-//        registerDefinitions(fields);
-//}
-
-//std::vector<const FieldInfo *>::const_iterator Definitions::findFieldIter(mip::CompositeDescriptor descriptor) const
-//{
-//    return std::lower_bound(
-//        mFields.begin(), mFields.end(), descriptor,
-//        [](const FieldInfo* info, CompositeDescriptor desc)->bool
-//        {
-//            return info->descriptor < desc;
-//        }
-//    );
-//}
 
 const DescriptorSetInfo* findDescriptorSet(const DescriptorSetSpan& descriptorSets, uint8_t descriptor)
 {
@@ -84,29 +58,5 @@ const FieldInfo* findField(const DescriptorSetInfo& ds_info, uint8_t field_desc)
     return (it != ds_info.fields.end()) && ((*it)->descriptor.fieldDescriptor==field_desc) ? *it : nullptr;
 }
 
-
-//
-//const FieldInfo* Definitions::findField(mip::CompositeDescriptor descriptor) const
-//{
-//    //auto it = findFieldIter(descriptor);
-//    auto it = mFields.find(descriptor);
-//
-//    if(it != mFields.end())
-//        return *it;
-//
-//    // Check for shared data fields.
-//    if(isDataDescriptorSet(descriptor.descriptorSet) && isSharedDataFieldDescriptor(descriptor.fieldDescriptor))
-//    {
-//        it = mFields.find(CompositeDescriptor{data_shared::DESCRIPTOR_SET, descriptor.fieldDescriptor});
-//
-//        if(it != mFields.end())
-//            return *it;
-//    }
-//    // Reply descriptor?
-//    else if(isCommandDescriptorSet(descriptor.descriptorSet) && isReplyFieldDescriptor(descriptor.fieldDescriptor))
-//        return &MetadataFor<ReplyField>::value;
-//
-//    return nullptr;
-//}
 
 } // namespace mip::metadata
--- src/cpp/mip/metadata/mip_definitions.hpp
+++ src/cpp/mip/metadata/mip_definitions.hpp
@@ -3,9 +3,6 @@
 #include <microstrain/common/span.hpp>
 #include <mip/mip_descriptors.hpp>
 
-//#include <set>
-//#include <initializer_list>
-
 
 namespace mip::metadata
 {
@@ -13,52 +10,10 @@ namespace mip::metadata
 struct FieldInfo;
 struct DescriptorSetInfo;
 
-using FieldSpan = microstrain::Span<const FieldInfo* const>;
 using DescriptorSetSpan = microstrain::Span<const DescriptorSetInfo* const>;
 
 const DescriptorSetInfo* findDescriptorSet(const DescriptorSetSpan& descriptorSets, uint8_t descriptor);
 const FieldInfo* findField(const DescriptorSetSpan& descriptorSets, mip::CompositeDescriptor descriptor);
 const FieldInfo* findField(const DescriptorSetInfo& ds_info, uint8_t field_desc);
-
-
-//class Definitions
-//{
-//    //struct Less
-//    //{
-//    //    inline bool operator()(const FieldInfo *a, const FieldInfo *b) const
-//    //    {
-//    //        return a->descriptor < b->descriptor;
-//    //    }
-//    //    inline bool operator()(const FieldInfo *a, CompositeDescriptor desc) const
-//    //    {
-//    //        return a->descriptor < desc;
-//    //    }
-//    //    inline bool operator()(CompositeDescriptor desc, const FieldInfo *a) const
-//    //    {
-//    //        return desc < a->descriptor;
-//    //    }
-//    //    using is_transparent = void;
-//    //};
-//    //
-//    //using Container = std::set<const FieldInfo *, Less>;
-//
-//public:
-//    Definitions() = default;
-//    Definitions(const DescriptorSetSpan& fields) { registerDefinitions(fields); }
-//
-//    //void registerField(const FieldInfo* field);
-//    //void registerDefinitions(const FieldSpan& fields);
-//    //void registerDefinitions(const DescriptorSetSpan& fields);
-//
-//    const DescriptorSetInfo* findDescriptorSet(uint8_t desc_set) const;
-//    const FieldInfo* findField(mip::CompositeDescriptor descriptor) const;
-//
-//private:
-//    //Container mFields;
-//    DescriptorSetSpan mDescriptorSets;
-//};
-
-
-
 
 } // namespace mip::metadata
