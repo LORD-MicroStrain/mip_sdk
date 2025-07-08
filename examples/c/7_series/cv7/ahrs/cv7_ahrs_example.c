0d384d7163ab8fa44bc676ea6f9e7eed09b184a3
Nick DaCosta <nick.dacosta@hbkworld.com>
2025-07-08 14:54:31 -0400

    Removed return statement in main function
    MSVC "cannot reach return" in release

--- examples/c/7_series/cv7/ahrs/cv7_ahrs_example.c
+++ examples/c/7_series/cv7/ahrs/cv7_ahrs_example.c
@@ -285,8 +285,6 @@ int main(const int argc, const char* argv[])
     }
 
     terminate(&device_port, "Example Completed Successfully.\n", true);
-
-    return 0;
 }
 
 // Custom logging handler callback
