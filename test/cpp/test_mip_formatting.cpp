251ce557a13d5f33a57639147ebef9aef2b9126b
Alex Farrell <ralexfarrell@outlook.com>
2025-09-04 12:23:18 -0400

    Converted rest of mip formatting tests to doctest

--- test/cpp/test_mip_formatting.cpp
+++ test/cpp/test_mip_formatting.cpp
@@ -3,80 +3,51 @@
 #include <mip/mip_logging.hpp>
 
 
-namespace fixture
-{
-    namespace detail
-    {
-        constexpr uint8_t SET_AND_SAVE_COMM_SPEED_PAYLOAD[] = {
-            0x75, 0x65, 0x01, 0x0c, 0x08, 0x09,
-            0x01, 0x01, 0x00, 0x01, 0xc2, 0x00,
-            0x04, 0x09, 0x03, 0x01,
-            0xCE, 0x80
-        };
-    };
-
-    enum class Action
-    {
-        SET_AND_SAVE_COMM_SPEED,
-    };
-
-    mip::PacketView getPacketForAction(const Action action)
-    {
-        switch(action)
-        {
-            case Action::SET_AND_SAVE_COMM_SPEED:
-            default:
-                return {detail::SET_AND_SAVE_COMM_SPEED_PAYLOAD};
-        }
-    }
+constexpr uint8_t SET_AND_SAVE_COMM_SPEED_PAYLOAD[] = {
+    0x75, 0x65, 0x01, 0x0c, 0x08, 0x09,
+    0x01, 0x01, 0x00, 0x01, 0xc2, 0x00,
+    0x04, 0x09, 0x03, 0x01,
+    0xCE, 0x80
 };
 
+TEST_SUITE_BEGIN("Formatting");
 
-TEST_CASE("Packet to bytes works")
+TEST_CASE("Formatting | A packet can be properly formatted to bytes")
 {
     char buffer[128];
     size_t index = 0;
-    mip::PacketView packet = fixture::getPacketForAction(fixture::Action::SET_AND_SAVE_COMM_SPEED);
 
-    bool ok = mip::formatPacketBytes(buffer, &index, packet);
+    bool ok = mip::formatPacketBytes(buffer, &index, {SET_AND_SAVE_COMM_SPEED_PAYLOAD});
 
     CHECK(ok);
     CHECK(index == 39);
     CHECK(strcmp(buffer, "7565010C 080901010001C200 04090301 CE80") == 0);
 }
 
-/*
-void format_packet_works()
+TEST_CASE("Formatting | A packet can be properly formatted to a human-readable string")
 {
     char buffer[128];
     size_t index = 0;
 
-    bool ok = mip::formatPacket(buffer, &index, {SET_SAVE_COMM_SPEED});
+    bool ok = mip::formatPacket(buffer, &index, {SET_AND_SAVE_COMM_SPEED_PAYLOAD});
 
-    TEST_ASSERT(ok, "Success");
-    TEST_ASSERT_EQ(index, 68, "Index is correct");
-    TEST_ASSERT_BUFFER_COMPARE(buffer, "Packet(DS=0x01){ Field(FD=0x09)[01010001C200] Field(FD=0x09)[0301] }", 68+1, "");
+    CHECK(ok);
+    CHECK(index == 68);
+    // TODO: Add assertion for strings that outputs the string values
+    CHECK(strcmp(buffer, "Packet(DS=0x01){ Field(FD=0x09)[01010001C200] Field(FD=0x09)[0301] }") == 0);
 }
 
-void format_field_works()
+TEST_CASE("Formatting | A field can be properly formatted to a human-readable string")
 {
     char buffer[128];
     size_t index = 0;
-    mip::FieldView field = mip::PacketView(SET_SAVE_COMM_SPEED).firstField();
+    const mip::FieldView field = mip::PacketView(SET_AND_SAVE_COMM_SPEED_PAYLOAD).firstField();
 
     bool ok = mip::formatField(buffer, &index, field);
 
-    TEST_ASSERT(ok, "Success");
-    TEST_ASSERT_EQ(index, 28, "Index is correct");
-    TEST_ASSERT_BUFFER_COMPARE(buffer, "Field(FD=0x09)[01010001C200]", 28+1, "");
+    CHECK(ok);
+    CHECK(index == 28);
+    CHECK(strcmp(buffer, "Field(FD=0x09)[01010001C200]") == 0);
 }
 
-int main()
-{
-    packet_to_bytes_works();
-    format_packet_works();
-    format_field_works();
-
-    return (int)g_fail_count;
-}
-*/
+TEST_SUITE_END();
