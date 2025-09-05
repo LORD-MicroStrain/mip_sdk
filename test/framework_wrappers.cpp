00403d51e73990717cea8b0119c46217d0dc451e
Alex Farrell <ralexfarrell@outlook.com>
2025-09-05 14:30:02 -0400

    Added source file for framework wrappers

---
+++ test/framework_wrappers.cpp
@@ -0,0 +1,28 @@
+#include "framework_wrappers.hpp"
+
+#include <cstring>
+#include <string>
+
+namespace detail
+{
+    void warn_c_strings_equal(const char* string1, const char* string2)
+    {
+        INFO(std::string(string1));
+        INFO(std::string(string2));
+        WARN_EQ(strcmp(string1, string2), 0);
+    }
+
+    void check_c_strings_equal(const char* string1, const char* string2)
+    {
+        INFO(std::string(string1));
+        INFO(std::string(string2));
+        CHECK_EQ(strcmp(string1, string2), 0);
+    }
+
+    void require_c_strings_equal(const char* string1, const char* string2)
+    {
+        INFO(std::string(string1));
+        INFO(std::string(string2));
+        REQUIRE_EQ(strcmp(string1, string2), 0);
+    }
+}
--- test/framework_wrappers.hpp
+++ test/framework_wrappers.hpp
@@ -1,33 +1,13 @@
 #pragma once
 
-#include <cstring>
-#include <string>
-
 #ifdef USE_DOCTEST
     #include <doctest/doctest.h>
 
     namespace detail
     {
-        inline void warn_c_strings_equal(const char* string1, const char* string2)
-        {
-            INFO(std::string(string1));
-            INFO(std::string(string2));
-            WARN_EQ(strcmp(string1, string2), 0);
-        }
-
-        inline void check_c_strings_equal(const char* string1, const char* string2)
-        {
-            INFO(std::string(string1));
-            INFO(std::string(string2));
-            CHECK_EQ(strcmp(string1, string2), 0);
-        }
-
-        inline void require_c_strings_equal(const char* string1, const char* string2)
-        {
-            INFO(std::string(string1));
-            INFO(std::string(string2));
-            REQUIRE_EQ(strcmp(string1, string2), 0);
-        }
+        void warn_c_strings_equal(const char* string1, const char* string2);
+        void check_c_strings_equal(const char* string1, const char* string2);
+        void require_c_strings_equal(const char* string1, const char* string2);
     }
 
     #define WARN_IF_NOT_TRUE(condition) WARN(condition)
