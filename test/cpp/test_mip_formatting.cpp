d9bcc550734a9f21261b2d503a426a83225d1978
Alex Farrell <ralexfarrell@outlook.com>
2025-09-05 14:19:41 -0400

    Added c string comparison wrappers

--- test/cpp/test_mip_formatting.cpp
+++ test/cpp/test_mip_formatting.cpp
@@ -16,11 +16,11 @@ TEST_CASE("Formatting | A packet can be properly formatted to bytes")
     char buffer[128];
     size_t index = 0;
 
-    bool ok = mip::formatPacketBytes(buffer, &index, {SET_AND_SAVE_COMM_SPEED_PAYLOAD});
+    const bool ok = mip::formatPacketBytes(buffer, &index, {SET_AND_SAVE_COMM_SPEED_PAYLOAD});
 
     EXPECT_TRUE(ok);
     EXPECT_TRUE(index == 39);
-    EXPECT_TRUE(strcmp(buffer, "7565010C 080901010001C200 04090301 CE80") == 0);
+    EXPECT_C_STRINGS_EQUAL(buffer, "7565010C 080901010001C200 04090301 CE80");
 }
 
 TEST_CASE("Formatting | A packet can be properly formatted to a human-readable string")
@@ -28,12 +28,11 @@ TEST_CASE("Formatting | A packet can be properly formatted to a human-readable s
     char buffer[128];
     size_t index = 0;
 
-    bool ok = mip::formatPacket(buffer, &index, {SET_AND_SAVE_COMM_SPEED_PAYLOAD});
+    const bool ok = mip::formatPacket(buffer, &index, {SET_AND_SAVE_COMM_SPEED_PAYLOAD});
 
     EXPECT_TRUE(ok);
     EXPECT_EQUAL(68, index);
-    // TODO: Add assertion for strings that outputs the string values
-    EXPECT_EQUAL(strcmp(buffer, "Packet(DS=0x01){ Field(FD=0x09)[01010001C200] Field(FD=0x09)[0301] }"), 0);
+    EXPECT_C_STRINGS_EQUAL(buffer, "Packet(DS=0x01){ Field(FD=0x09)[01010001C200] Field(FD=0x09)[0301] }");
 }
 
 TEST_CASE("Formatting | A field can be properly formatted to a human-readable string")
@@ -42,11 +41,11 @@ TEST_CASE("Formatting | A field can be properly formatted to a human-readable st
     size_t index = 0;
     const mip::FieldView field = mip::PacketView(SET_AND_SAVE_COMM_SPEED_PAYLOAD).firstField();
 
-    bool ok = mip::formatField(buffer, &index, field);
+    const bool ok = mip::formatField(buffer, &index, field);
 
     EXPECT_TRUE(ok);
     EXPECT_TRUE(index == 28);
-    EXPECT_TRUE(strcmp(buffer, "Field(FD=0x09)[01010001C200]") == 0);
+    EXPECT_C_STRINGS_EQUAL(buffer, "Field(FD=0x09)[01010001C200]");
 }
 
 TEST_SUITE_END();
--- test/framework_wrappers.hpp
+++ test/framework_wrappers.hpp
@@ -1,13 +1,44 @@
 #pragma once
 
+#include <cstring>
+#include <string>
+
 #ifdef USE_DOCTEST
     #include <doctest/doctest.h>
+
+    namespace detail
+    {
+        inline void warn_c_strings_equal(const char* string1, const char* string2)
+        {
+            INFO(std::string(string1));
+            INFO(std::string(string2));
+            WARN_EQ(strcmp(string1, string2), 0);
+        }
+
+        inline void check_c_strings_equal(const char* string1, const char* string2)
+        {
+            INFO(std::string(string1));
+            INFO(std::string(string2));
+            CHECK_EQ(strcmp(string1, string2), 0);
+        }
+
+        inline void require_c_strings_equal(const char* string1, const char* string2)
+        {
+            INFO(std::string(string1));
+            INFO(std::string(string2));
+            REQUIRE_EQ(strcmp(string1, string2), 0);
+        }
+    }
 
     #define WARN_IF_NOT_TRUE(condition) WARN(condition)
     #define EXPECT_TRUE(condition)      CHECK(condition)
     #define ASSERT_TRUE(condition)      REQUIRE(condition)
 
-    #define WARN_IF_NOT_EQUAL(expected, actual) WARN_EQ(expected, actual)
-    #define EXPECT_EQUAL(expected, actual) CHECK_EQ(expected, actual)
-    #define ASSERT_EQUAL(expected, actual) REQUIRE_EQ(expected, actual)
+    #define WARN_IF_NOT_EQUAL(value1, value2) WARN_EQ(value1, value2)
+    #define EXPECT_EQUAL(value1, value2)      CHECK_EQ(value1, value2)
+    #define ASSERT_EQUAL(value1, value2)      REQUIRE_EQ(value1, value2)
+
+    #define WARN_IF_C_STRINGS_NOT_EQUAL(value1, value2) detail::warn_c_strings_equal(value1, value2)
+    #define EXPECT_C_STRINGS_EQUAL(value1, value2)      detail::check_c_strings_equal(value1, value2)
+    #define ASSERT_C_STRINGS_EQUAL(value1, value2)      detail::require_c_strings_equal(value1, value2)
 #endif
