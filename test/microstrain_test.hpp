e3c5aee8fba059c8e8dca2486791ddeeb2a8fb1a
Alex Farrell <ralexfarrell@outlook.com>
2025-09-09 15:47:42 -0400

    Unindented regular code in preprocessor #ifdefs

--- test/microstrain_test.hpp
+++ test/microstrain_test.hpp
@@ -10,33 +10,32 @@
 #pragma once
 
 #ifdef USE_DOCTEST
-    #include <doctest/doctest.h>
+#include <doctest/doctest.h>
 
-    namespace detail
-    {
-        void warn_c_strings_equal(const char* string1, const char* string2);
-        void check_c_strings_equal(const char* string1, const char* string2);
-        void require_c_strings_equal(const char* string1, const char* string2);
-    }
+namespace detail
+{
+    void warn_c_strings_equal(const char* string1, const char* string2);
+    void check_c_strings_equal(const char* string1, const char* string2);
+    void require_c_strings_equal(const char* string1, const char* string2);
+}
 
-    // Using Doctest's tagging system here so we can run specific test suites without
-    // having to use TEST_SUITE_BEGIN/TEST_SUITE_END.
-    #define TEST(suite_name, test_name) TEST_CASE("[" suite_name "] " test_name)
+// Using Doctest's tagging system here so we can run specific test suites without
+// having to use TEST_SUITE_BEGIN/TEST_SUITE_END.
+#define TEST(suite_name, test_name) TEST_CASE("[" suite_name "] " test_name)
 
-    #define WARN_IF_NOT_TRUE(condition) WARN(condition)
-    #define EXPECT_TO_BE_TRUE(condition) CHECK(condition)
-    #define ASSERT_TO_BE_TRUE(condition) REQUIRE(condition)
+#define WARN_IF_NOT_TRUE(condition) WARN(condition)
+#define EXPECT_TO_BE_TRUE(condition) CHECK(condition)
+#define ASSERT_TO_BE_TRUE(condition) REQUIRE(condition)
 
-    #define WARN_IF_NOT_EQUAL(value1, value2) WARN_EQ(value1, value2)
-    #define EXPECT_TO_BE_EQUAL(value1, value2) CHECK_EQ(value1, value2)
-    #define ASSERT_TO_BE_EQUAL(value1, value2) REQUIRE_EQ(value1, value2)
+#define WARN_IF_NOT_EQUAL(value1, value2) WARN_EQ(value1, value2)
+#define EXPECT_TO_BE_EQUAL(value1, value2) CHECK_EQ(value1, value2)
+#define ASSERT_TO_BE_EQUAL(value1, value2) REQUIRE_EQ(value1, value2)
 
-    #define WARN_IF_C_STRINGS_NOT_EQUAL(value1, value2) detail::warn_c_strings_equal(value1, value2)
-    #define EXPECT_C_STRINGS_TO_BE_EQUAL(value1, value2) detail::check_c_strings_equal(value1, value2)
-    #define ASSERT_C_STRINGS_TO_BE_EQUAL(value1, value2) detail::require_c_strings_equal(value1, value2)
+#define WARN_IF_C_STRINGS_NOT_EQUAL(value1, value2) detail::warn_c_strings_equal(value1, value2)
+#define EXPECT_C_STRINGS_TO_BE_EQUAL(value1, value2) detail::check_c_strings_equal(value1, value2)
+#define ASSERT_C_STRINGS_TO_BE_EQUAL(value1, value2) detail::require_c_strings_equal(value1, value2)
 
-    #define WARN_IF_BUFFER_NOT_TERMINATED_AT_POSITION(position, buffer) WARN_EQ('\0', buffer[position])
-    #define EXPECT_BUFFER_TO_BE_TERMINATED_AT_POSITION(position, buffer) CHECK_EQ('\0', buffer[position])
-    #define ASSERT_BUFFER_TO_BE_TERMINATED_AT_POSITION(position, buffer) REQUIRE_EQ('\0', buffer[position])
-
+#define WARN_IF_BUFFER_NOT_TERMINATED_AT_POSITION(position, buffer) WARN_EQ('\0', buffer[position])
+#define EXPECT_BUFFER_TO_BE_TERMINATED_AT_POSITION(position, buffer) CHECK_EQ('\0', buffer[position])
+#define ASSERT_BUFFER_TO_BE_TERMINATED_AT_POSITION(position, buffer) REQUIRE_EQ('\0', buffer[position])
 #endif
