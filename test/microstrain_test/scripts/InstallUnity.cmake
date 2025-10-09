e52ec1e8809768df0b624fe6c15d9513e1260df8
Alex Farrell <ralexfarrell@outlook.com>
2025-10-09 15:23:13 -0400

    Added installation script for unity and added option to enable it for microstrain_test

---
+++ test/microstrain_test/scripts/InstallUnity.cmake
@@ -0,0 +1,15 @@
+include(FetchContent)
+
+FetchContent_Declare(unity
+    GIT_REPOSITORY "https://github.com/ThrowTheSwitch/Unity.git"
+    GIT_TAG "v2.6.1"
+)
+
+FetchContent_MakeAvailable(unity)
+
+# Unity doesn't have native CMake support, so we have to build it manually
+if (NOT TARGET unity)
+    add_library(unity STATIC ${unity_SOURCE_DIR}/src/unity.c)
+
+    target_include_directories(unity PUBLIC ${unity_SOURCE_DIR}/src)
+endif()
