0004740bd8ea051fee416643519279143554d39a
Alex Farrell <ralexfarrell@outlook.com>
2025-10-13 13:10:12 -0400

    Switch C++ integration tests to use new test registration macro

--- test/cpp/mip.integration.cpp
+++ test/cpp/mip.integration.cpp
@@ -54,7 +54,7 @@ bool packetCallback(void*, const PacketView *parsedPacket, Timestamp timestamp)
     return true;
 }
 
-UNIT_TEST("Packet Builder", "Packets can be built and parsed correctly")
+INTEGRATION_TEST("Packet Builder", "Packets can be built and parsed correctly")
 {
     std::random_device random_device;
     std::mt19937 random_generator(random_device());
