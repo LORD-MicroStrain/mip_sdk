fe5a678d4958a6f0f549c49b8041c3ac72c60e14
Alex Farrell <ralexfarrell@outlook.com>
2025-10-16 12:33:43 -0400

    Disabled spectre mitigation warnings for Unity

--- test/microstrain_test/scripts/InstallUnity.cmake
+++ test/microstrain_test/scripts/InstallUnity.cmake
@@ -10,6 +10,11 @@ FetchContent_MakeAvailable(unity)
 # Unity doesn't have native CMake support, so we have to build it manually
 if (NOT TARGET unity)
     add_library(unity STATIC ${unity_SOURCE_DIR}/src/unity.c)
-
     target_include_directories(unity PUBLIC ${unity_SOURCE_DIR}/src)
+endif()
+
+# Disable Spectre mitigation warnings for Unity as building with /Qspectre would add performance
+# overhead without a significant security threat to justify it.
+if(MSVC)
+    target_compile_options(unity PRIVATE /wd5045)
 endif()
