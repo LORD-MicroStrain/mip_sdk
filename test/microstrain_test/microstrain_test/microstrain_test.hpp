04559b2d5123568c6fcf2d28386ea2f3cc49c15f
Alex Farrell <ralexfarrell@outlook.com>
2025-10-21 16:48:40 -0400

    Updated documentation for microstrain test C++

--- test/microstrain_test/microstrain_test/microstrain_test.hpp
+++ test/microstrain_test/microstrain_test/microstrain_test.hpp
@@ -7,13 +7,18 @@
     #include "doctest_wrappers.hpp"
 #endif
 
-
 // -----------------------------------------------------------------------------------------------------------
-// Test registration
+// Automatic test registration and discovery
 // -----------------------------------------------------------------------------------------------------------
 
 /// @brief Registers a test case in the given test suite.
 ///
-/// The test suite will be created if it doesn't already exist.
+/// All tests in a test suite can be run together by passing the suite name as
+/// a label to CTest. This currently only works with automatic test discovery,
+/// or if you set the label for a CTest to the suite name manually.
+///
+/// @param suite_name A string, which may contain any characters.
+/// @param test_name A string, which may contain any characters.
+///
 #define MICROSTRAIN_TEST_CASE(suite_name, test_name) \
     MICROSTRAIN_TEST_CASE_IMPL(suite_name, test_name)
