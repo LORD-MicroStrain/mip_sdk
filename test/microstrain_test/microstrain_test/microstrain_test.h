04c9bf30bef73b4fb49e034b76601dee26979f9a
Alex Farrell <ralexfarrell@outlook.com>
2025-10-21 16:22:09 -0400

    Updated documentation for MicrostrainTest C API

--- test/microstrain_test/microstrain_test/microstrain_test.h
+++ test/microstrain_test/microstrain_test/microstrain_test.h
@@ -7,39 +7,59 @@
     #include "unity_wrappers.h"
 #endif
 
+// -----------------------------------------------------------------------------------------------------------
+// Automatic test registration
+// -----------------------------------------------------------------------------------------------------------
 
 /// @brief Registers a test case in the given test suite.
 ///
-/// The test suite will be created if it doesn't already exist.
+/// All tests in a test suite can be run together by passing the suite name as
+/// a label to CTest. This currently only works with automatic test discovery,
+/// or if you set the label for a CTest to the suite name manually.
+///
+/// @param suite_name Currently needs to be a valid function identifier.
+/// @param test_name Currently needs to be a valid function identifier.
 ///
 #define MICROSTRAIN_TEST_CASE(suite_name, test_name) \
     MICROSTRAIN_TEST_CASE_IMPL(suite_name, test_name)
 
+// -----------------------------------------------------------------------------------------------------------
+// Manual test registration
+// -----------------------------------------------------------------------------------------------------------
+
+/// @brief Completes any required setup before tests can be run.
+///
+/// DON'T use this directly if using automatic test discovery. This should be called
+/// near the top of the main file, before the main function definition.
+///
 #define MICROSTRAIN_TEST_DEFAULT_SETUP() \
     MICROSTRAIN_TEST_DEFAULT_SETUP_IMPL()
 
-/// @brief Call this to begin the test registration process.
+/// @brief Begins the test registration process.
 ///
-/// Don't use this directly if using automatic test discovery.
-///
-/// Register tests after this call. When done, call the END function.
+/// DON'T use this directly if using automatic test discovery. If manually discovering
+/// tests, register tests after this call in the main function. When done, call the
+/// END macro.
 ///
 #define MICROSTRAIN_TEST_BEGIN() \
     MICROSTRAIN_TEST_BEGIN_IMPL()
 
-/// @brief Call this to end the test registration process.
+/// @brief Ends the test registration process.
 ///
-/// Don't use this directly if using automatic test discovery.
-///
-/// No tests will be registered after this call. The result should be returned from the main function.
+/// DON'T use this directly if using automatic test discovery. If manually discovering
+/// tests, no tests will be registered after this call in the main function. The result
+/// should be returned from the main function.
 ///
 #define MICROSTRAIN_TEST_END() \
     MICROSTRAIN_TEST_END_IMPL()
 
 /// @brief Runs a MicrostrainTest-defined test case.
 ///
-/// Call this in a main function if not using automatic test discovery. This only needs to be
-/// called if not using automatic test discovery.
+/// DON'T use this directly if using automatic test discovery. Call this in the main
+/// function for each test to run.
+///
+/// @param suite_name Currently needs to be a valid function identifier.
+/// @param test_name Currently needs to be a valid function identifier.
 ///
 #define RUN_MICROSTRAIN_TEST_CASE(suite_name, test_name) \
     RUN_MICROSTRAIN_TEST_CASE_IMPL(suite_name, test_name)
@@ -48,6 +68,6 @@
 // Internals
 // -----------------------------------------------------------------------------------------------------------
 
-/// @brief This is a modified version to work with automatic test discovery, don't use directly!
+/// @brief DON'T use directly! This is a modified version to work with automatic test discovery.
 #define INTERNAL_RUN_MICROSTRAIN_TEST_CASE_AUTO_DISCOVER(suite_name, test_name, file_path) \
     INTERNAL_RUN_MICROSTRAIN_TEST_CASE_AUTO_DISCOVER_IMPL(suite_name, test_name, file_path)
